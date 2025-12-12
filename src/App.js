20cf2f43efeb53bd433651a38461504c030f11df
@sudhaelango_94 <sudhaelango.94@gmail.com>
2025-12-12 10:38:28 +0530

    first added

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React from "react";
+class App extends React.Component
+{
+  render()
+  {
+    return(<>
+    <h1>welcome to components</h1>
+    <h1>welcome to components</h1>
+    <h1>welcome to components</h1>
+    <h1>welcome to components</h1>
+    <p>Elit ad ipsum officia magna. Id cillum do ex aliqua nulla occaecat eiusmod consequat ad. Reprehenderit cupidatat quis laborum nulla et dolor ex reprehenderit minim nisi ipsum.Quis id incididunt deserunt aute ex velit est veniam tempor. Veniam reprehenderit culpa voluptate voluptate. Et tempor labore incididunt laboris elit commodo do est eu sint cupidatat. Exercitation consequat fugiat do velit est nisi quis do minim.
+
+Deserunt deserunt dolore irure Lorem tempor laboris tempor veniam officia nisi officia. Incididunt eiusmod commodo amet et incididunt enim deserunt ullamco sunt labore nostrud ea enim laboris. Anim mollit minim ex in sunt quis non aliquip adipisicing excepteur consectetur excepteur. Adipisicing pariatur velit veniam irure cillum id culpa eiusmod officia. Veniam ullamco ad Lorem velit laborum voluptate sint cillum.
+
+Laboris in velit excepteur laborum velit est commodo exercitation commodo officia sunt veniam proident. Sit velit pariatur aliqua nulla exercitation sunt fugiat enim nostrud duis. Tempor esse quis incididunt ipsum in magna do officia voluptate quis.
+
+
+</p>
+    </>)
+  }
+}
+export default App
+/*function App()
+{
+  return(
+    <div>
+     <h1>welcome to react</h1>
+     <h1>welcome to react</h1>
+     <h1>welcome to react</h1>
+     </div>*/
+   
+    
+
+
+// export default App
+/*
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+*/
